19b871a4d1b42e995102a3927d808251f27bb301
Das <me@dasliao.com>
2022-04-17 01:11:16 -0700

    Initial Commit

---
+++ MindAlert/MindAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindAlertApp.swift
+//  MindAlert
+//
+//  Created by DASHAN LIAO on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MindAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
